00949ad503abf2c5397e4db74f60278d16b8d5fc
alexgaya <alejandrogaya1994@gmail.com>
2019-03-06 17:51:12 +0100

    improved performance on add and remove song

--- src/app/services/list.service.ts
+++ src/app/services/list.service.ts
@@ -16,6 +16,46 @@ export class ListService {
     this.lists = listsJson;
   }
 
+  private updateNsongs(listIndex: number): void {
+    this.lists[listIndex].n_songs = this.lists[listIndex].songs.length;
+  }
+
+  private updateDuration(listIndex: number): void {
+    this.lists[listIndex].duration = 0;
+    this.lists[listIndex].songs.forEach(s => {
+      this.lists[listIndex].duration += s.duration;
+    });
+  }
+
+  private checkIfSongsAlreadyExists(list: List, song: Song): boolean {
+    let alreadyExists: boolean = false;
+
+    /*this.lists.forEach(l => {
+      if (l == list) {
+        l.songs.forEach(s => {
+          if (song.id == s.id) {
+            alreadyExists = true;
+          }
+        });
+      }
+    });*/
+
+    let i = 0;
+    let j = 0;
+    while (!alreadyExists && i < this.lists.length) {
+      if (this.lists[i] == list) {
+        while (!alreadyExists && j < this.lists[i].songs.length) {
+          if (song.id == this.lists[i].songs[j].id) {
+            alreadyExists = true;
+          }
+          j++;
+        }
+      }
+      i++;
+    }
+    return alreadyExists;
+  }
+
   public setSelectedList(list: List): void {
     this.selectedList = list;
   }
@@ -39,17 +79,6 @@ export class ListService {
 
   public getLists(): List[] {
     return this.lists;
-  }
-
-  private updateNsongs(listIndex: number): void {
-    this.lists[listIndex].n_songs = this.lists[listIndex].songs.length;
-  }
-
-  private updateDuration(listIndex: number): void {
-    this.lists[listIndex].duration = 0;
-    this.lists[listIndex].songs.forEach(s => {
-      this.lists[listIndex].duration += s.duration;
-    });
   }
 
   public removeSong(list: List, song: Song) {
@@ -81,35 +110,6 @@ export class ListService {
       }
       i++;
     }
-  }
-
-  private checkIfSongsAlreadyExists(list: List, song: Song): boolean {
-    let alreadyExists: boolean = false;
-
-    /*this.lists.forEach(l => {
-      if (l == list) {
-        l.songs.forEach(s => {
-          if (song.id == s.id) {
-            alreadyExists = true;
-          }
-        });
-      }
-    });*/
-
-    let i = 0;
-    let j = 0;
-    while (!alreadyExists && i < this.lists.length) {
-      if (this.lists[i] == list) {
-        while (!alreadyExists && j < this.lists[i].songs.length) {
-          if (song.id == this.lists[i].songs[j].id) {
-            alreadyExists = true;
-          }
-          j++;
-        }
-      }
-      i++;
-    }
-    return alreadyExists;
   }
 
   public addSong(list: List, song: Song): void {
